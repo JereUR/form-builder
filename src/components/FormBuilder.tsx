7ec5066f3e05063aed0dc328cf613daac2b5fd6f
JereUR <jeremias.jdv@gmail.com>
2024-04-11 21:20:18 -0300

    feat(Root): Dragging sensors added

--- src/components/FormBuilder.tsx
+++ src/components/FormBuilder.tsx
@@ -1,5 +1,11 @@
 'use client'
-import { DndContext } from '@dnd-kit/core'
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
 
 import { Form } from '@prisma/client'
 import PreviewDialogBtn from './PreviewDialogBtn'
@@ -9,8 +15,24 @@ import Designer from './Designer'
 import DragOverlayWrapper from './DragOverlayWrapper'
 
 function FormBuilder({ form }: { form: Form }) {
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10
+    }
+  })
+
+  //Sensor for mobile devices
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 300,
+      tolerance: 5
+    }
+  })
+
+  const sensors = useSensors(mouseSensor, touchSensor)
+
   return (
-    <DndContext>
+    <DndContext sensors={sensors}>
       <main className="flex flex-col w-full">
         <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
           <h2 className="truncate font-medium">
